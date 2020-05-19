949f8ae69b21db717ff23304c4aba864868b3db5
Rohan Nimje <r.c.nimje10@gmail.com>
2020-05-19 17:09:05 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,133 @@
+
+window.onload = function() {
+}
+
+var map;
+var markers = [];
+var infoWindow;
+
+function initMap() {
+    var India = {
+        lat: 19.663280,
+        lng: 75.300293
+    };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: India,
+        zoom: 11,
+        mapTypeId: 'roadmap',
+    });
+    infoWindow = new google.maps.InfoWindow();
+    searchStores();
+}
+
+function searchStores(){
+    var foundStores = [];
+    var address = document.getElementById('address-input').value;
+    if(address){
+        for(var store of stores){
+            var postal = store['address'];
+            if(postal == address){
+                foundStores.push(store);
+            }
+        }
+    } else {
+        foundStores = stores;
+    }
+    clearLocations();
+    displayStores(foundStores);
+    showStoresMarkers(foundStores);
+    setOnClickListener();
+}
+
+function clearLocations(){
+    infoWindow.close();
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    }
+    markers.length = 0;
+}
+
+function setOnClickListener(){
+    var storeElements = document.querySelectorAll('.store-container');
+    storeElements.forEach(function(elem, index){
+        elem.addEventListener('click', function(){
+            new google.maps.event.trigger(markers[index], 'click');
+        })
+    })
+}
+
+function displayStores(stores){
+    var storesHtml = '';
+    for(var [index, store] of stores.entries()){
+        var address = store['address'];
+        var phone = store['TigerNumber'];
+        storesHtml += `
+            <div class="store-container">
+                <div class="store-container-background">
+                    <div class="store-info-container">
+                        <div class="store-address">
+                            <span>${address[0]}</span>
+                            <span>${address[1]}</span>
+                        </div>
+                        <div class="store-phone-number">${phone}</div>
+                    </div>
+                    <div class="store-number-container">
+                        <div class="store-number">
+                            ${index+1}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+        document.querySelector('.stores-list').innerHTML = storesHtml;
+    }
+}
+
+
+function showStoresMarkers(stores){
+    var bounds = new google.maps.LatLngBounds();
+    for(var [index, store] of stores.entries()){
+        var latlng = new google.maps.LatLng(
+            store["coordinates"]["latitude"],
+            store["coordinates"]["longitude"]);
+        var name = store["name"];
+        var address = store["address"][1];
+        var TigerNumber = store["TigerNumber"];
+        bounds.extend(latlng);
+        createMarker(latlng, name, address, TigerNumber, index+1);
+    }
+    map.fitBounds(bounds);
+}
+
+function createMarker(latlng, name, address, TigerNumber, index){
+    var html = `
+        <div class="store-info-window">
+            <div class="store-info-name">
+                ${name}
+            </div>
+
+            <div class="store-info-address">
+                <div class="circle">
+                    <i class="fas fa-location-arrow"></i>
+                </div>
+                ${address}
+            </div>
+            <div class="store-info-phone">
+                <div class="circle">
+                    <i class="fas fa-paw"></i>
+                </div>
+                ${TigerNumber}
+            </div>
+        </div>
+    `;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: latlng,
+      label: index.toString()
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      infoWindow.setContent(html);
+      infoWindow.open(map, marker);
+    });
+    markers.push(marker);
+}
---
+++ js/store-data.js
@@ -0,0 +1,678 @@
+let stores = [{
+    "recommendation": {},
+    "name": "Bandipur Reserve (1973)",
+    "TigerNumber": "TIGERS : 120",
+    "coordinates": {
+        "latitude": 11.666400,
+        "longitude": 76.629189
+    },
+    "regulations": [],
+    "address": ["Bandipur National Park (1973)", "Karnataka, IN "],
+    },
+
+    {
+    "recommendation": {},
+    "name": "Corbett Tiger Reserve (1973)",
+    "TigerNumber": "TIGERS : 215",
+    "coordinates": {
+        "latitude": 29.548599,
+        "longitude": 78.935303
+    },
+    "regulations": [],
+    "address": ["Jim Corbett National Park (1973)", "Uttarakhand, IN"],
+
+    },
+
+
+    {
+    "recommendation": {},
+    "name": "Kanha Tiger Reserve (1973)",
+    "TigerNumber": "TIGERS : 80",
+    "coordinates": {
+        "latitude": 22.283255,
+        "longitude": 80.601119
+    },
+    "address": ["Kanha Tiger Reserve (1973)", "Madhya Pradesh, IN"],
+    },
+
+
+
+    {
+    "recommendation": {},
+    "name": "Manas Tiger Reserve (1973)",
+    "TigerNumber": "TIGERS : 11",
+    "coordinates": {
+        "latitude": 22.146710,
+        "longitude": 88.825260
+    },
+
+
+    "address": ["Manas Wildlife Sanctuary (1973)", "Assam, IN"],
+
+},
+
+
+{
+    "recommendation": {},
+    "name": "Melghat Tiger Reserve (1973)",
+    "TigerNumber": "TIGERS : 25",
+    "coordinates": {
+        "latitude": 21.406,
+        "longitude": 77.1485
+    },
+
+    "address": ["Melghat Tiger Reserve (1973)", "Maharashtra, IN"],
+    },
+
+    {
+    "recommendation": {},
+    "name": "Palamau Tiger Reserve (1973)",
+    "TigerNumber": "TIGERS : 3",
+    "coordinates": {
+        "latitude": 23.68889,
+        "longitude": 84.24889
+    },
+
+    "address": ["Palamau Tiger Reserve (1973)", "Jharkhand, IN"],
+
+    },
+
+
+    {
+    "recommendation": {},
+    "name": "Ranthambore Tiger Reserve (1973)",
+    "TigerNumber": "TIGERS : 37",
+    "coordinates": {
+        "latitude": 26.01733,
+        "longitude": 76.50257
+    },
+    "address": ["Ranthambore National Park (1973)", "Rajasthan, IN"],
+    },
+
+
+
+    {
+    "recommendation": {},
+    "name": "Similipal Tiger Reserve",
+    "TigerNumber": "TIGERS : 3",
+    "coordinates": {
+        "latitude": 21.7500,
+        "longitude": 86.3333
+    },
+
+
+    "address": ["Similipal National Park (1973)", "Odisha, IN"],
+
+},
+
+
+{
+    "recommendation": {},
+    "name": "Sunderbans National Park",
+    "TigerNumber": "TIGERS : 68",
+    "coordinates": {
+        "latitude": 21.9333,
+        "longitude":  88.8500
+    },
+
+    "address": ["Sunderbans National Park (1973)", "West Bengal, IN"],
+    },
+
+    {
+    "recommendation": {},
+    "name": "Periyar National Park (1978)",
+    "TigerNumber": "TIGERS : 20",
+    "coordinates": {
+        "latitude": 9.4622,
+        "longitude": 77.2368
+    },
+
+    "address": ["Periyar National Park (1978)", "Kerala, IN"],
+
+    },
+
+
+    {
+    "recommendation": {},
+    "name": "Sariska Tiger Reserve (1978)",
+    "TigerNumber": "TIGERS : 9",
+    "coordinates": {
+        "latitude": 27.3285,
+        "longitude": 76.4332
+    },
+    "address": ["Sariska Tiger Reserve (1978)", "Rajasthan, IN"],
+    },
+
+
+
+    {
+    "recommendation": {},
+    "name": "Buxa Tiger Reserve",
+    "TigerNumber": "TIGERS : 2",
+    "coordinates": {
+        "latitude": 26.6473,
+        "longitude": 89.5483
+    },
+
+
+    "address": ["Buxa Tiger Reserve (1982)", "West Bengal, IN"],
+
+},
+
+
+{
+    "recommendation": {},
+    "name": "Indravati National Park",
+    "TigerNumber": "TIGERS : 12",
+    "coordinates": {
+        "latitude": 19.2059,
+        "longitude": 81.0313
+    },
+
+    "address": ["Indravati National Park (1982)", "Chattisgarh, IN"],
+    },
+
+    {
+    "recommendation": {},
+    "name": "Namdapha National Park",
+    "TigerNumber": "TIGERS : 11",
+    "coordinates": {
+        "latitude": 27.023,
+        "longitude": 96.015
+    },
+
+    "address": ["Namdapha National Park (1982)", "Arunachal Pradesh, IN"],
+
+    },
+
+
+    {
+    "recommendation": {},
+    "name": "Dudhwa National Park (1987)",
+    "TigerNumber": "TIGERS : 58",
+    "coordinates": {
+        "latitude": 28.4346,
+        "longitude": 80.5703
+    },
+    "address": ["Dudhwa National Park (1987)", "Uttar Pradesh, IN"],
+    },
+
+
+
+    {
+    "recommendation": {},
+    "name": "Kalakkad Mundanthurai Tiger Reserve (1988)",
+    "TigerNumber": "TIGERS : 10",
+    "coordinates": {
+        "latitude": 8.5049,
+        "longitude": 77.4664
+    },
+
+
+    "address": ["Kalakkad Mundanthurai Tiger Reserve (1988)", "Tamil Nadu, IN"],
+
+},
+
+
+{
+    "recommendation": {},
+    "name": "Valmiki National Park (1989)",
+    "TigerNumber": "TIGERS : 40",
+    "coordinates": {
+        "latitude": 27.3800,
+        "longitude": 84.1435
+    },
+
+    "address": ["Valmiki National Park (1989)", "Bihar, IN"],
+    },
+
+    {
+    "recommendation": {},
+    "name": "Pench Tiger Reserve (1992)",
+    "TigerNumber": "TIGERS : 43",
+    "coordinates": {
+        "latitude": 22.016617,
+        "longitude": 79.829674
+    },
+
+    "address": ["Pench Tiger Reserve (1992)", "Madhya Pradesh, IN"],
+
+    },
+
+
+    {
+    "recommendation": {},
+    "name": "Tadoba Andhari Tiger Reserve (1993)",
+    "TigerNumber": "TIGERS : 51",
+    "coordinates": {
+        "latitude": 20.248467,
+        "longitude": 79.425378
+    },
+    "address": ["Tadoba Andhari Tiger Reserve (1993)", "Maharashtra, IN"],
+    },
+
+
+
+    {
+    "recommendation": {},
+    "name": "Bandhavgarh National Park (1993)",
+    "TigerNumber": "TIGERS : 63",
+    "coordinates": {
+        "latitude": 23.722454,
+        "longitude": 81.024219
+    },
+
+
+    "address": ["Bandhavgarh National Park (1993)", "Madhya Pradesh, IN"],
+
+},
+
+
+{
+    "recommendation": {},
+    "name": "Panna National Park (1994)",
+    "TigerNumber": "TIGERS : 17",
+    "coordinates": {
+        "latitude": 24.5905,
+        "longitude": 79.9417
+    },
+
+    "address": ["Panna National Park (1994)", "Madhya Pradesh, IN"],
+    },
+
+    {
+    "recommendation": {},
+    "name": "Dampa Tiger Reserve (1994)",
+    "TigerNumber": "TIGERS : 3",
+    "coordinates": {
+        "latitude": 23.5034,
+        "longitude": 92.4180
+    },
+
+    "address": ["Dampa Tiger Reserve (1994)", "Mizoram, IN"],
+
+    },
+
+
+    {
+    "recommendation": {},
+    "name": "Bhadra Wildlife Sanctuary (1998)",
+    "TigerNumber": "TIGERS : 22",
+    "coordinates": {
+        "latitude": 13.6949,
+        "longitude": 75.6352
+    },
+    "address": ["Bhadra Wildlife Sanctuary (1998)", "Karnataka, IN"],
+    },
+
+
+
+    {
+    "recommendation": {},
+    "name": "Pench National Park (1998)",
+    "TigerNumber": "TIGERS : 35",
+    "coordinates": {
+        "latitude": 22.016617,
+        "longitude": 79.829674
+    },
+
+
+    "address": ["Pench National Park (1998)", "Maharashtra, IN"],
+
+},
+
+
+{
+    "recommendation": {},
+    "name": "Pakke Tiger Reserve (1999)",
+    "TigerNumber": "TIGERS : 7",
+    "coordinates": {
+        "latitude": 19.663280,
+        "longitude": 75.300293
+    },
+
+    "address": ["Pakke Tiger Reserve (1999)", "Arunachal Pradesh, IN"],
+    },
+
+    {
+    "recommendation": {},
+    "name": "Nameri National Park (1999)",
+    "TigerNumber": "TIGERS : 5",
+    "coordinates": {
+        "latitude": 27.0060,
+        "longitude": 92.7873
+    },
+
+    "address": ["Nameri National Park (1999)", "Assam, IN"],
+
+    },
+
+
+    {
+    "recommendation": {},
+    "name": "Satpura Tiger Reserve (1999)",
+    "TigerNumber": "TIGERS : 26",
+    "coordinates": {
+        "latitude": 21.9833,
+        "longitude": 74.8667
+    },
+    "address": ["Satpura Tiger Reserve (1999)", "Madhya Pradesh, IN"],
+    },
+
+
+
+    {
+    "recommendation": {},
+    "name": "Aanaimalai Tiger Reserve (2008)",
+    "TigerNumber": "TIGERS : 13",
+    "coordinates": {
+        "latitude": 19.663280,
+        "longitude": 75.300293
+    },
+
+
+    "address": ["Aanaimalai Tiger Reserve (2008)", "Tamil Nadu, IN"],
+
+},
+
+
+{
+    "recommendation": {},
+    "name": "Udanti-Sitanadi Wildlife Sanctuary (2008)",
+    "TigerNumber": "TIGERS : 4",
+    "coordinates": {
+        "latitude": 20.4554,
+        "longitude": 81.9753
+    },
+
+    "address": ["Udanti-Sitanadi Wildlife Sanctuary (2008)", "Chhattisgarh, IN"],
+    },
+
+    {
+    "recommendation": {},
+    "name": "Satkosia Tiger Reserve (2008)",
+    "TigerNumber": "TIGERS : 3",
+    "coordinates": {
+        "latitude": 20.5071,
+        "longitude": 84.8662
+    },
+
+    "address": ["Satkosia Tiger Reserve (2008)", "	Odisha, IN"],
+
+    },
+
+
+    {
+    "recommendation": {},
+    "name": "Kaziranga National Park (2008)",
+    "TigerNumber": "TIGERS : 103",
+    "coordinates": {
+        "latitude": 26.5775,
+        "longitude": 93.1711
+    },
+    "address": ["Kaziranga National Park (2008)", "Assam, IN"],
+    },
+
+
+
+    {
+    "recommendation": {},
+    "name": "Achanakmar Wildlife Sanctuary (2008)",
+    "TigerNumber": "TIGERS : 11",
+    "coordinates": {
+        "latitude": 22.5052,
+        "longitude": 81.7787
+    },
+
+
+    "address": ["Achanakmar Wildlife Sanctuary (2008)", "Chhattisgarh, IN"],
+
+},
+
+
+{
+    "recommendation": {},
+    "name": "Kali Tiger Reserve (2008)",
+    "TigerNumber": "TIGERS : 5",
+    "coordinates": {
+        "latitude": 14.9981,
+        "longitude": 74.3587
+    },
+
+    "address": ["Kali Tiger Reserve (2008)", "Karnataka, IN"],
+    },
+
+    {
+    "recommendation": {},
+    "name": "Sanjay National Park (2008)",
+    "TigerNumber": "TIGERS : 8",
+    "coordinates": {
+        "latitude": 24.4212,
+        "longitude": 81.8723
+    },
+
+    "address": ["Sanjay National Park (2008)", "Madhya Pradesh, IN"],
+
+    },
+
+
+    {
+    "recommendation": {},
+    "name": "Mudumalai National Park (2008)",
+    "TigerNumber": "TIGERS : 89",
+    "coordinates": {
+        "latitude": 11.5623,
+        "longitude": 76.5345
+    },
+    "address": ["Mudumalai National Park (2008)", "Tamil Nadu, IN"],
+    },
+
+
+
+    {
+    "recommendation": {},
+    "name": "Nagarhole National Park (2008)",
+    "TigerNumber": "TIGERS : 101",
+    "coordinates": {
+        "latitude": 12.0314,
+        "longitude": 76.1207
+    },
+
+
+    "address": ["Nagarhole National Park (2008)", "Karnataka, IN"],
+
+},
+
+
+{
+    "recommendation": {},
+    "name": "Parambikulam Tiger Reserve (2008)",
+    "TigerNumber": "TIGERS : 19",
+    "coordinates": {
+        "latitude": 10.4470,
+        "longitude": 76.8157
+    },
+
+    "address": ["Parambikulam Tiger Reserve (2008)", "Kerala, IN"],
+    },
+
+    {
+    "recommendation": {},
+    "name": "Sahyadri Tiger Reserve (2009)",
+    "TigerNumber": "TIGERS : 7",
+    "coordinates": {
+        "latitude": 17.4862,
+        "longitude": 73.8093
+    },
+
+    "address": ["Sahyadri Tiger Reserve (2009)", "Maharashtra, IN"],
+
+    },
+
+
+    {
+    "recommendation": {},
+    "name": "Biligiri Ranganatha Temple Reserve (2010)",
+    "TigerNumber": "TIGERS : 68",
+    "coordinates": {
+        "latitude": 12.000732,
+        "longitude": 77.138403
+    },
+    "address": ["Biligiri Ranganatha Temple Reserve (2010)", "Karnataka, IN"],
+    },
+
+
+
+    {
+    "recommendation": {},
+    "name": "Kawal Tiger Reserve (2012)",
+    "TigerNumber": "TIGERS : 12",
+    "coordinates": {
+        "latitude": 19.1189,
+        "longitude": 78.9990
+    },
+
+
+    "address": ["Kawal Tiger Reserve (2012)", "Telangana, IN"],
+
+},
+
+
+{
+    "recommendation": {},
+    "name": "Sathyamangalam Tiger Reserve (2013)",
+    "TigerNumber": "TIGERS : 72",
+    "coordinates": {
+        "latitude": 11.5816,
+        "longitude": 76.9989
+    },
+
+    "address": ["Sathyamangalam Tiger Reserve (2013)", "Tamil Nadu, IN"],
+    },
+
+    {
+    "recommendation": {},
+    "name": "Mukundara Hills Tiger Reserve (2013)",
+    "TigerNumber": "TIGERS : 50",
+    "coordinates": {
+        "latitude": 24.7965,
+        "longitude": 76.0085
+    },
+
+    "address": ["Mukundara Hills Tiger Reserve (2013)", "Rajasthan, IN"],
+
+    },
+
+
+    {
+        "recommendation": {},
+        "name": "Navegaon National Park (2013)",
+        "TigerNumber": "TIGERS : 7",
+        "coordinates": {
+            "latitude": 20.8627,
+            "longitude":  80.2229
+        },
+
+
+        "address": ["Navegaon National Park (2013)", "Maharashtra, IN"],
+
+    },
+
+
+    {
+    "recommendation": {},
+    "name": "Nagarjunsagar-Srisailam Tiger Reserve (2013)",
+    "TigerNumber": "TIGERS : 74",
+    "coordinates": {
+        "latitude":16.0304,
+        "longitude": 78.8904
+    },
+    "address": ["Nagarjunsagar-Srisailam Tiger Reserve (2013)", "‎Andhra Pradesh‎ & Telangana, IN"],
+    },
+
+
+
+
+    {
+    "recommendation": {},
+    "name": "Amrabad Tiger Reserve (2014)",
+    "TigerNumber": "",
+    "coordinates": {
+        "latitude": 16.3557,
+        "longitude": 78.8335
+    },
+
+    "address": ["Amrabad Tiger Reserve (2014)", "Telangana, IN"],
+    },
+
+    {
+    "recommendation": {},
+    "name": "Pilibhit Tiger Reserve (2014)",
+    "TigerNumber": "TIGERS : 25",
+    "coordinates": {
+        "latitude": 28.6887,
+        "longitude": 80.0767
+    },
+
+    "address": ["Pilibhit Tiger Reserve (2014)", "	Uttar Pradesh, IN"],
+
+    },
+
+
+    {
+    "recommendation": {},
+    "name": "Bor Tiger Reserve (2014)",
+    "TigerNumber": "TIGERS : 5",
+    "coordinates": {
+        "latitude": 20.9791,
+        "longitude": 78.6658
+    },
+    "address": ["Bor Tiger Reserve (2014)", "Maharashtra, IN"],
+    },
+
+
+
+    {
+    "recommendation": {},
+    "name": "Rajaji National Park (2015)",
+    "TigerNumber": "",
+    "coordinates": {
+        "latitude": 29.708309,
+        "longitude": 78.384460
+    },
+
+
+    "address": ["Rajaji National Park (2015)", "Uttarakhand, IN"],
+
+},
+
+
+{
+    "recommendation": {},
+    "name": "Orang National Park (2016)",
+    "TigerNumber": "",
+    "coordinates": {
+        "latitude": 26.5568,
+        "longitude": 92.3279
+    },
+    "address": ["Orang National Park (2016)", "Assam, IN"],
+    },
+
+
+
+    {
+    "recommendation": {},
+    "name": "Kamlang Wildlife Sanctuary (2016)",
+    "TigerNumber": "TIGERS : 1",
+    "coordinates": {
+        "latitude": 22.146710,
+        "longitude":  88.825260
+    },
+
+
+    "address": ["Kamlang Wildlife Sanctuary (2016)", "Arunachal Pradesh, IN"],
+
+},
+
+
+]
